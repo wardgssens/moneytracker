43bdcd8151450daf757b45d5726aefce1eddae79
@wardgssens <wardgoossens@gmail.com>
2020-12-11 00:32:37 +0100

    Cleaned up Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,47 +9,16 @@ import view.ViewFrame;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractFactory<Person> personFactory = FactoryProvider.getFactory("person");
-        AbstractFactory<Ticket> ticketFactory = FactoryProvider.getFactory("ticket");
-
-        // Create ticketdatabase.
+        // Model
         TicketDatabase ticketdb = TicketDatabase.getInstance();
-
-        // Create persondatabase.
         PersonDatabase persondb = PersonDatabase.getInstance();
 
-        // Create person and put in db.
-        Person a = personFactory.create("individual", "Orval");
-        persondb.addPerson(a);
-        Person b = personFactory.create("individual", "Alfredo");
-        persondb.addPerson(b);
-        Person c = personFactory.create("individual", "Andreas");
-        persondb.addPerson(c);
-        Person d = personFactory.create("individual", "Duane");
-        persondb.addPerson(d);
-        Person everyone = personFactory.create("everyone", "");
-
-        // Create tickets and put in db.
-        Ticket temp = ticketFactory.create("airplane", "tickets to Ibiza");
-        temp.addEntry(500, a, b);
-        ticketdb.addTicket(temp);
-
-        temp = ticketFactory.create("movie", "baywatch with the boys");
-        temp.addEntry(20, a, b);
-        ticketdb.addTicket(temp);
-
-        temp = ticketFactory.create("restaurant", "lunch");
-        temp.addEntry(100, c, everyone);
-        ticketdb.addTicket(temp);
-
-        temp = ticketFactory.create("restaurant", "dinner");
-        temp.addEntry(20, d, c);
-        ticketdb.addTicket(temp);
-
+        // View
         ViewFrame frame = new ViewFrame();
-
         persondb.addObserver(frame);
         ticketdb.addObserver(frame);
-        Controller controller = new Controller(persondb, ticketdb, frame);
+
+        // Controller
+        new Controller(persondb, ticketdb, frame);
     }
 }
